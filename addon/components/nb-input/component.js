01ae1c95954617c46503395ab62149207d22daca
nullrocket <doug.hill@passpack.com>
2017-03-15 11:41:50 -0700

    Initial Commit

--- addon/components/nb-input/component.js
+++ addon/components/nb-input/component.js
@@ -24,7 +24,7 @@ var uniqID = {
 export default Ember.Component.extend(ThemedComponent, {
   gestures: Ember.inject.service(),
   layout: layout,
-
+  autocomplete:"off",
   _themeProperties: [
     'attrs.focused-underline-color',
     'attrs.underline-color',
